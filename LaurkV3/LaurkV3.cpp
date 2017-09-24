c0527146295131f1a34f74fd27b2421296b34d0a
LAUSTEN-GAMERIG\User <laurie.f.austen@btinternet.com>
2017-09-24 01:56:35 +0100

    Parsing 'look' verb.

--- LaurkV3/LaurkV3.cpp
+++ LaurkV3/LaurkV3.cpp
@@ -20,50 +20,57 @@ enum ERooms {
 	AmmoStoreS,
 	Dorm,
 	Lab};
+const int ROOMSNo = 12;
 enum EDirections {North, East, South, West};
 const int NONE = -1;
-const int ROOMS = 12;
-const int DIRECTIONS = 4;
+const int DIRECTIONSNo = 4;
 struct Room 
 {
+	std::string Name;
 	std::string Description;
-	int Exits[DIRECTIONS];
+	int Exits[DIRECTIONSNo];
 };
 void SetRooms(Room*);
 
 //User input handling
-bool GetCommand(std::string, std::string&, std::string&);
+enum EVerbs {Inventory, Look, Use, Get, Drop, Open, Close, Help};
+const int VERBSNo = 8;
 struct Word
 {
 	std::string word;
 	int Code;
 };
+bool GetCommand(std::string, std::string&, std::string&);
 void SetDirections(Word*);
-bool Parse(int& OUTLocation, std::string, std::string, Word*, Room*);
+void SetVerbs(Word*);
+bool Parse(int& OUTLocation, std::string, std::string, Word* Directions,Word* Verbs, Room*);
+void LookAround(int Location, Room*, Word*);
 
 int main()
 {
 	GameIntro();
 	std::string Input, Word1, Word2;
 	//Create array of rooms and set their values.
-	Room Rooms[ROOMS];
+	Room Rooms[ROOMSNo];
 	SetRooms(Rooms);
 	int Location = Reception;
 	std::cout << "You wake up in " << Rooms[Location].Description << std::endl;
 	//Create any words needed for parsing input.
-	Word Directions[DIRECTIONS];
+	Word Directions[DIRECTIONSNo];
 	SetDirections(Directions);
+	Word Verbs[VERBSNo];
+	SetVerbs(Verbs);
 
 	while (Word1 != "QUIT")
 	{
 		Input.clear();
-		std::cout << "What next o wise one?\n";
+		std::cout << "What next o' wise one?\n";
 		std::getline(std::cin, Input);
 		Word1.clear();
 		Word2.clear();
 		GetCommand(Input, Word1, Word2);
 
-		Parse(Location, Word1, Word2, Directions, Rooms);
+		Parse(Location, Word1, Word2, Directions, Verbs, Rooms);
 	}
 	return 0;
 }
@@ -145,72 +152,84 @@ bool GetCommand(std::string Input, std::string& OUTWord1, std::string& OUTWord2)
 
 void SetRooms(Room* Room)
 {
+	Room[Hangar].Name.assign("a cavernous hangar.");
 	Room[Hangar].Description.assign("a huge hangar with a couple of disued helicopters, there is an opening in the roof.");
 	Room[Hangar].Exits[North] = NONE;
 	Room[Hangar].Exits[East] = NONE;
 	Room[Hangar].Exits[South] = CorridoorN;
 	Room[Hangar].Exits[West] = NONE;
 
+	Room[Freezer].Name.assign("a walk-in freezer.");
 	Room[Freezer].Description.assign("the walk-in freezer, brimming with food.");
 	Room[Freezer].Exits[North] = NONE;
 	Room[Freezer].Exits[East] = Kitchen;
 	Room[Freezer].Exits[South] = NONE;
 	Room[Freezer].Exits[West] = NONE;
 
+	Room[Kitchen].Name.assign("an industrial scale kitchen.");
 	Room[Kitchen].Description.assign("a wide kitchen, stocked with everything needed to feed an army.");
 	Room[Kitchen].Exits[North] = NONE;
 	Room[Kitchen].Exits[East] = CorridoorN;
 	Room[Kitchen].Exits[South] = NONE;
 	Room[Kitchen].Exits[West] = Freezer;
 
+	Room[CorridoorN].Name.assign("the corridoor.");
 	Room[CorridoorN].Description.assign("the northen end of a long corridoor.");
 	Room[CorridoorN].Exits[North] = Hangar;
 	Room[CorridoorN].Exits[East] = NONE;
 	Room[CorridoorN].Exits[South] = CorridoorS;
 	Room[CorridoorN].Exits[West] = Kitchen;
-
+	
+	Room[Office].Name.assign("an office.");
 	Room[Office].Description.assign("a grand office with a large mahogany desk.");
 	Room[Office].Exits[North] = NONE;
 	Room[Office].Exits[East] = Reception;
 	Room[Office].Exits[South] = NONE;
 	Room[Office].Exits[West] = NONE;
 
+	Room[Reception].Name.assign("the reception hall.");
 	Room[Reception].Description.assign("a previously well-decorated reception, now lying in disrepair.");
 	Room[Reception].Exits[North] = NONE;
 	Room[Reception].Exits[East] = AmmoStoreN;
 	Room[Reception].Exits[South] = NONE;
 	Room[Reception].Exits[West] = Office;
 
+	Room[AmmoStoreN].Name.assign("a dark warehouse.");
 	Room[AmmoStoreN].Description.assign("the northern end of a long warehouse stacked with boxes of rifles and armour.");
 	Room[AmmoStoreN].Exits[North] = NONE;
 	Room[AmmoStoreN].Exits[East] = CorridoorS;
 	Room[AmmoStoreN].Exits[South] = AmmoStoreS;
 	Room[AmmoStoreN].Exits[West] = Reception;
 
+	Room[CorridoorS].Name.assign("the corridoor.");
 	Room[CorridoorS].Description.assign("the southern end of a long corridoor.");
 	Room[CorridoorS].Exits[North] = CorridoorN;
 	Room[CorridoorS].Exits[East] = NONE;
 	Room[CorridoorS].Exits[South] = Dorm;
 	Room[CorridoorS].Exits[West] = AmmoStoreN;
 
+	Room[MaintRoom].Name.assign("a maintenance room.");
 	Room[MaintRoom].Description.assign("a sweltering maintenance room full of steam and whirring machines.");
 	Room[MaintRoom].Exits[North] = NONE;
 	Room[MaintRoom].Exits[East] = AmmoStoreS;
 	Room[MaintRoom].Exits[South] = NONE;
 	Room[MaintRoom].Exits[West] = NONE;
 
+	Room[AmmoStoreS].Name.assign("a dark warehouse.");
 	Room[AmmoStoreS].Description.assign("the southern end of a long warehouse filled with crates of ammo and munitions.");
 	Room[AmmoStoreS].Exits[North] = AmmoStoreN;
 	Room[AmmoStoreS].Exits[East] = NONE;
 	Room[AmmoStoreS].Exits[South] = Lab;
 	Room[AmmoStoreS].Exits[West] = MaintRoom;
 
+	Room[Dorm].Name.assign("a cramped dormitory.");
 	Room[Dorm].Description.assign("a dorm lined with bunks with a row of lockers at the end of the room.");
 	Room[Dorm].Exits[North] = CorridoorS;
 	Room[Dorm].Exits[East] = NONE;
 	Room[Dorm].Exits[South] = NONE;
 	Room[Dorm].Exits[West] = NONE;
 
+	Room[Lab].Name.assign("a well hidden laboratory.");
 	Room[Lab].Description.assign("a laboratory brimming with equipment and secrets.");
 	Room[Lab].Exits[North] = AmmoStoreS;
 	Room[Lab].Exits[East] = NONE;
@@ -230,9 +249,29 @@ void SetDirections(Word* Direction)
 	Direction[West].word = "WEST";
 }
 
-bool Parse(int& OUTLocation, std::string Word1, std::string Word2, Word* Directions, Room* Rooms)
+void SetVerbs(Word* Verbs)
 {
-	for (int i = 0; i < DIRECTIONS; i++) 
+	Verbs[Inventory].Code = Inventory;
+	Verbs[Inventory].word = "INVENTORY";
+	Verbs[Look].Code = Look;
+	Verbs[Look].word = "LOOK";
+	Verbs[Use].Code = Use;
+	Verbs[Use].word = "USE";
+	Verbs[Get].Code = Get;
+	Verbs[Get].word = "GET";
+	Verbs[Drop].Code = Drop;
+	Verbs[Drop].word = "DROP";
+	Verbs[Open].Code = Open;
+	Verbs[Open].word = "OPEN";
+	Verbs[Close].Code = Close;
+	Verbs[Close].word = "CLOSE";
+	Verbs[Help].Code = Help;
+	Verbs[Help].word = "Help";
+}
+
+bool Parse(int& OUTLocation, std::string Word1, std::string Word2, Word* Directions, Word* Verbs, Room* Rooms)
+{
+	for (int i = 0; i < DIRECTIONSNo; i++) 
 	{
 		if (Word1 == Directions[i].word) 
 		{
@@ -249,6 +288,38 @@ bool Parse(int& OUTLocation, std::string Word1, std::string Word2, Word* Directi
 			}
 		}
 	}
+	int VerbCode = NONE;
+	for (int i = 0; i < VERBSNo; i++) 
+	{
+		if (Word1 == Verbs[i].word) 
+		{
+			VerbCode = Verbs[i].Code;
+			break;
+		}
+	}
+	if (VerbCode == Look) 
+	{
+		LookAround(OUTLocation, Rooms, Directions);
+		return true;
+	}
 	std::cout << "No valid command recognised, type help if youre stuck.\n";
 	return false;
+}
+
+void LookAround(int Location, Room* Rooms, Word* Directions)
+{
+	std::cout << "You are in " << Rooms[Location].Description << std::endl;
+	for (int i = 0; i < DIRECTIONSNo; i++) 
+	{
+		if (Rooms[Location].Exits[i] != NONE)
+		{
+			std::cout << "There is an exit ";
+			for (int j = 0; j < Directions[i].word.size(); j++)
+			{
+				char Lower = tolower(Directions[i].word[j]);
+				std::cout << Lower;
+			}
+			std::cout << " to " << Rooms[Rooms[Location].Exits[i]].Name << std::endl;
+		}
+	}
 }
